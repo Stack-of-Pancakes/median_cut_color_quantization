1c5ee8e992403e57a39bfa621dc78e0dcc50686c
Stack-of-Pancakes <angrybisquick@gmail.com>
2018-07-20 16:27:36 -0400

    Add option to create true median cut, or fast cut using unique colors

--- median_cut.py
+++ median_cut.py
@@ -1,4 +1,5 @@
 from statistics import mean
+from PIL import Image
 
 
 class ColorCube(object):
@@ -28,10 +29,21 @@ class ColorCube(object):
         return self.max_range < other.max_range
 
 
-def median_cut(img, num_colors):
+def median_cut(img, num_colors, unique=False):
+    # If unique is true then multiple instances of a single RGB value will only be counted once
+    # and the rest discarded. This is MUCH faster and creates a more diverse pallete, but is not
+    # a "true" median cut.
+    # For example if an image had 99 blue pixels (0,0,255) and a single red pixel (255,0,0) the
+    # respective median cuts would be
+    # unique = False: [(0,0,255),(5,0,249)]
+    # unique = True:  [(0,0,255),(255,0,0)]
+    # False would produce 2 almost identical shades of blue, True would result in pure blue/red
     colors = []
     for color_count, color in img.getcolors(img.width * img.height):
-        colors += [color] * color_count
+        if unique:
+            colors += [color]
+        else:
+            colors += [color] * color_count
     cubes = [ColorCube(colors)]
 
     while len(cubes) < num_colors:
@@ -39,3 +51,23 @@ def median_cut(img, num_colors):
         cubes += cubes.pop().split()
 
     return [c.average() for c in cubes]
+
+
+def show_median_cut(cuts):
+    # Create background for the palette
+    palette = Image.new('RGB', (100 * len(cuts), 100))
+
+    # Create a square of each color and insert each, side-by-side, into the palette
+    for i in range(len(cuts)):
+        color = Image.new('RGB', (100, 100), cuts[i])
+        palette.paste(color, (100 * i, 0))
+
+    palette.show()
+
+
+my_image = Image.open('input/cogs.jpg')
+
+# Create two palettes.  One with true median cut and the other with unique colors.
+median_non_unique = median_cut(my_image, 8)
+median_unique = median_cut(my_image, 8, unique=True)
+
